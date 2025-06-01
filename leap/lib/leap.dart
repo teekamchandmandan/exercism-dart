846b8f39f9ef27feb67b30c9c9d3d14e59b13777
Teekam <teekamchandmandan@gmail.com>
2025-06-01 14:28:47 +0530

    https://exercism.org/tracks/dart/exercises/leap

---
+++ leap/lib/leap.dart
@@ -0,0 +1,11 @@
+class Leap {
+  bool leapYear(int year) {
+    if (year%100 == 0 && year%400 != 0) {
+      return false;
+    }
+    if (year % 4 != 0) {
+      return false;
+    } 
+    return true;
+  }
+}
