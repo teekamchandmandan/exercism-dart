857cfd8cb8bf8d9f843ce96377d39ec48de85bb2
Teekam <teekamchandmandan@gmail.com>
2025-06-03 12:04:37 +0530

    https://exercism.org/tracks/dart/exercises/atbash-cipher

---
+++ atbash-cipher/lib/atbash_cipher.dart
@@ -0,0 +1,44 @@
+class AtbashCipher {
+  encode(String message) {
+    final buffer = StringBuffer();
+    for (int i = 0; i < message.length; i++) {
+      String curr = message[i].toLowerCase();
+      if (curr == ' ' || curr == ',' || curr == '.') {
+        continue;
+      }
+      if ((buffer.length + 1) % 6 == 0) {
+        buffer.write(' ');
+      }
+      int complement = 'z'.codeUnitAt(0) - curr.codeUnitAt(0);
+      if (complement < 0 || complement > 26) {
+        buffer.write(curr);
+      } else {
+        buffer.write(String.fromCharCode('a'.codeUnitAt(0) + complement));
+      }
+    }
+    return buffer.toString();
+  }
+
+  decode(String code) {
+    final buffer = StringBuffer();
+    for (int i = 0; i < code.length; i++) {
+      String curr = code[i].toLowerCase();
+      if (curr == ' ' || curr == ',' || curr == '.') {
+        continue;
+      }
+      int complement = 'z'.codeUnitAt(0) - curr.codeUnitAt(0);
+      if (complement < 0 || complement > 26) {
+        buffer.write(curr);
+      } else {
+        buffer.write(String.fromCharCode('a'.codeUnitAt(0) + complement));
+      }
+    }
+    return buffer.toString();
+  }
+}
+
+void main() {
+  var obj = AtbashCipher();
+  print(obj.encode('The quick brown fox jumps over the lazy dog.'));
+  print(obj.decode('gvhgr mt123 gvhgr mt'));
+}
